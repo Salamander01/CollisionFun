cbb431ca28b0e41cd2fb3d9ec7d61bfe7121cc7c
Salamander01 <sfaerber24@icloud.com>
2023-02-27 15:00:12 -0700

    Make Vector.java

--- src/Vector.java
+++ src/Vector.java
@@ -1,3 +1,26 @@
 public class Vector {
 
+    private double componentX;
+    private double componentY;
+
+    public Vector(double componentX, double componentY) {
+        this.componentX = componentX;
+        this.componentY = componentY;
+    }
+
+    public Vector add(Vector vector) {
+        return new Vector(this.componentX + vector.componentX, this.componentY + vector.componentY);
+    }
+
+    public double getComponentX() {
+        return componentX;
+    }
+
+    public double getComponentY() {
+        return componentY;
+    }
+
+    public static Vector fromAngleMagnitude(double angle, double magnitude) {
+        return new Vector(magnitude * Math.cos(angle), magnitude * Math.sin(angle));
+    }
 }
